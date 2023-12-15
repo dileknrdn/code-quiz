df71ea3c784c7abb28eb8a94d1a1a644e38fbd70
Dilek Nuredin <dileknuredin@hotmail.com>
2023-12-15 11:30:06 -0500

    added test questions to star app

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+const questions = [ 
+    {
+        question: "what is the biggest continent in the world?", 
+        answers: ["Europe", "Asia", "America", "Africa"],
+        correct: "Africa"
+    }, 
+    {
+        question: "what is the biggest country in the world?", 
+        answers: ["China", "Russia", "USA", "Germany"],
+        correct: "Russia"
+    },
+    {
+        question: "what is the country with the biggest coffe production in the world?", 
+        answers: ["Brazil", "Asia", "America", "Africa"],
+        correct: "Brazil" 
+    },
+];
