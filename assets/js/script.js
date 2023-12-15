9be044e008faf27c6d857e8e6a8fe1b876baed46
Dilek Nuredin <dileknuredin@hotmail.com>
2023-12-15 11:54:47 -0500

    added function show questions to display question on html

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,21 @@ const formSection=document.querySelector(".form-section")
 const inputEl= document.getElementById("initials-input")
 const submitBtn=document.getElementById("submit-btn")
 
+const index=0
 
+function showQuestions(){
+    
+    questionEL.textContent=questions[index].question
+
+    for (let i = 0; i < questions[index].answers.length; i++) {
+        const btn= document.createElement("button")
+        btn.setAttribute("class", "btn")
+        btn.textContent=questions[index].answers[i]
+
+        btnList.append(btn)
+        
+    }
+}
 
 
 
@@ -16,4 +30,5 @@ const submitBtn=document.getElementById("submit-btn")
 startBtn.addEventListener("click", ()=>{
     startSection.classList.add("hide")
     questionSection.classList.remove("hide")
+    showQuestions()
 })
