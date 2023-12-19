b86f6f91d37a8f5f2f705781361c3f293ba3471f
Dilek Nuredin <dileknuredin@hotmail.com>
2023-12-18 23:23:36 -0500

    added elements to CSS, Updated HTML and JavaScript

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,49 +1,74 @@
-const questions = [ 
-    {
-        question: "________ is the process of finding errors and fixing them within a program.", 
-        answers: ["Compiling", "Executing", "Debugging", "Scanning"],
-        correct:"Debugging"
-    }, 
-    {
-        question: "During program development, software requirements specify______", 
-        answers: ["How the program will accomplish the task", "What the task is that the program must perform", "How to divide the task into subtasks", "How to test the program when it is done"],
-        correct:"What the task is that the program must perform"
-    },
-    {
-        question: "A loop that never ends is referred to as a(n)_________.", 
-        answers: ["While loop", "Infinite loop", "Recursive loop", "For loop"],
-        correct:"Infinite loop" 
-    },
-    {
-        question: "Which command will stop an infinite loop?", 
-        answers: ["Alt - C", "Shift - C", "Esc", "Ctrl - C"],
-        correct:"Ctrl - C" 
-    },{
-        question: "What does HTML stand for?", 
-        answers: ["Hyper Trainer Marking Language", "Hyper Text Marketing Language", "Hyper Text Markup Language", "Hyper Text Markup Leveler"],
-        correct:"Hyper Text Markup Language" 
-    },{
-        question: "____________It defines the common variables and methods of a set of objects", 
-        answers: ["Objects", "Class", "Function", "Method"],
-        correct:"Class" 
-    },{
-        question: "What do mean by incrementing?", 
-        answers: ["Squaring", "Adding one", "Subtracting one", "Setting initial value"],
-        correct:"Adding one" 
-    },{
-        question: "HTML files are divided into 2 parts: the head and the ________.", 
-        answers: ["Attribute", "Body", "Tittle", "HTML"],
-        correct:"Body" 
-    },
-    {
-        question: "Href attribute stands for__________.", 
-        answers: ["Hyperlink refrence", "Hyperlink referral", "Hyperlink", "Hyperlink Referencing"],
-        correct:"Hyperlink reference" 
-    },
-    {
-        question: "____________it is the code/s inside a pair of curly braces.", 
-        answers: ["Block", "Brick", "Function", "Method"],
-        correct:"Block" 
-    },
-
+const questions = [
+  {
+    question:
+      "________ is the process of finding errors and fixing them within a program.",
+    answers: ["Compiling", "Executing", "Debugging", "Scanning"],
+    correct: "Debugging",
+  },
+  {
+    question: "During program development, software requirements specify______",
+    answers: [
+      "How the program will accomplish the task",
+      "What the task is that the program must perform",
+      "How to divide the task into subtasks",
+      "How to test the program when it is done",
+    ],
+    correct: "What the task is that the program must perform",
+  },
+  {
+    question: "A loop that never ends is referred to as a(n)_________.",
+    answers: ["While loop", "Infinite loop", "Recursive loop", "For loop"],
+    correct: "Infinite loop",
+  },
+  {
+    question: "Which command will stop an infinite loop?",
+    answers: ["Alt - C", "Shift - C", "Esc", "Ctrl - C"],
+    correct: "Ctrl - C",
+  },
+  {
+    question: "What does HTML stand for?",
+    answers: [
+      "Hyper Trainer Marking Language",
+      "Hyper Text Marketing Language",
+      "Hyper Text Markup Language",
+      "Hyper Text Markup Leveler",
+    ],
+    correct: "Hyper Text Markup Language",
+  },
+  {
+    question:
+      "____________It defines the common variables and methods of a set of objects",
+    answers: ["Objects", "Class", "Function", "Method"],
+    correct: "Class",
+  },
+  {
+    question: "What do mean by incrementing?",
+    answers: [
+      "Squaring",
+      "Adding one",
+      "Subtracting one",
+      "Setting initial value",
+    ],
+    correct: "Adding one",
+  },
+  {
+    question: "HTML files are divided into 2 parts: the head and the ________.",
+    answers: ["Attribute", "Body", "Tittle", "HTML"],
+    correct: "Body",
+  },
+  {
+    question: "Href attribute stands for__________.",
+    answers: [
+      "Hyperlink refrence",
+      "Hyperlink referral",
+      "Hyperlink",
+      "Hyperlink Referencing",
+    ],
+    correct: "Hyperlink reference",
+  },
+  {
+    question: "____________it is the code/s inside a pair of curly braces.",
+    answers: ["Block", "Brick", "Function", "Method"],
+    correct: "Block",
+  },
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,60 @@
-const startSection= document.querySelector(".start-box")
-const startBtn=document.getElementById("start-btn")
-const questionSection= document.querySelector(".question-section")
-const questionEL= document.getElementById("question")
-const btnList=document.querySelector(".btn-list")
-const formSection=document.querySelector(".form-section")
-const inputEl= document.getElementById("initials-input")
-const submitBtn=document.getElementById("submit-btn")
+const startSection = document.querySelector(".start-box");
+const startBtn = document.getElementById("start-btn");
+const questionSection = document.querySelector(".question-section");
+const questionEL = document.getElementById("question");
+const btnList = document.querySelector(".btn-list");
+const formSection = document.querySelector(".form-section");
+const inputEl = document.getElementById("initials-input");
+const submitBtn = document.getElementById("submit-btn");
 
-let questionindex=0
-let score= 0
+let questionindex = 0;
+let score = 0;
 
-function showQuestions(){
-    if(questionindex=== questions.length){
-        endQuiz()
-    }
-    questionEL.textContent=questions[questionindex].question
-    btnList.innerHTML=""
-    for (let i = 0; i < questions[questionindex].answers.length; i++) {
-        const btn= document.createElement("button")
-        btn.setAttribute("class", "btn")
-        btn.textContent=questions[questionindex].answers[i]
+let count = 0;
+var intId = setInterval(counter, 60000);
 
-        btnList.append(btn)
-        
-    }
+function counter() {
+  console.log(--count);
+}
+function showQuestions() {
+  if (questionindex === questions.length) {
+    endQuiz();
+  }
+  questionEL.textContent = questions[questionindex].question;
+  btnList.innerHTML = "";
+  for (let i = 0; i < questions[questionindex].answers.length; i++) {
+    const btn = document.createElement("button");
+    btn.setAttribute("class", "btn");
+    btn.textContent = questions[questionindex].answers[i];
+
+    btnList.append(btn);
+  }
 }
 
-function checkAnswer(answer){
-
-    if(answer === questions[questionindex].correct){
-        
-        questionindex++
-        score++
-        showQuestions()
-    }else{
-        questionindex++
-        showQuestions()
-        //take time out of timer
-    }
+function checkAnswer(answer) {
+  if (answer === questions[questionindex].correct) {
+    questionindex++;
+    score++;
+    showQuestions();
+  } else {
+    questionindex++;
+    showQuestions();
+    //take time out of timer
+  }
 }
 
-function endQuiz(){
-    questionSection.classList.add("hide")
-    formSection.classList.remove("hide")
+function endQuiz() {
+  questionSection.classList.add("hide");
+  formSection.classList.remove("hide");
 }
 
+startBtn.addEventListener("click", () => {
+  startSection.classList.add("hide");
+  questionSection.classList.remove("hide");
+  showQuestions();
+});
 
-
-startBtn.addEventListener("click", ()=>{
-    startSection.classList.add("hide")
-    questionSection.classList.remove("hide")
-    showQuestions()
-})
-
-btnList.addEventListener("click", ()=>{
-    const userChoice= this.event.target.textContent
-    checkAnswer(userChoice);
-})
+btnList.addEventListener("click", () => {
+  const userChoice = this.event.target.textContent;
+  checkAnswer(userChoice);
+});
